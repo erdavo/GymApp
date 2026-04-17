4e91c4890e241a548af01d1a9a1714b7ba33e6af
David <doto0992@gmail.com>
2026-04-17 21:02:37 +0200

    feat: implement GymController to future index page with Mustache templates

--- src/main/java/com/gymapp/gymapp/Controller/GymController.java
+++ src/main/java/com/gymapp/gymapp/Controller/GymController.java
@@ -4,11 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.gymapp.gymapp.Service.ExerciseService;
+import com.gymapp.gymapp.Service.RoutineService;
+
 @Controller
 public class GymController {
 
+    private final ExerciseService exerciseService;
+    private final RoutineService routineService;
+    // TODO: private final TrainerService trainerService;
+
+    // TODO: add TrainerService trainerService in constructor
+    public GymController(ExerciseService exerciseService, RoutineService routineService) {
+        this.exerciseService = exerciseService;
+        this.routineService = routineService;
+        // TODO: add this.trainerService = trainerService;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        return "index"; 
+        model.addAttribute("exercises", exerciseService.getAllExercises());
+        model.addAttribute("routines", routineService.getAllRoutines());
+        // TODO: model.addAttribute("trainers", trainerService.getAllTrainers());
+
+        return "index";
     }
 }
