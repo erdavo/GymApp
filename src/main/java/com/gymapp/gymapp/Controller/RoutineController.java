b5abb175f7c1d0a56e154608776e6f544f02abb2
David <doto0992@gmail.com>
2026-04-17 20:40:51 +0200

    feat: add controller for routines

---
+++ src/main/java/com/gymapp/gymapp/Controller/RoutineController.java
@@ -0,0 +1,68 @@
+package com.gymapp.gymapp.Controller;
+
+import com.gymapp.gymapp.Model.Routine;
+import com.gymapp.gymapp.Service.RoutineService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Map;
+
+@RestController
+public class RoutineController {
+    private final RoutineService routineService;
+
+    public RoutineController(RoutineService routineService) {
+        this.routineService = routineService;
+    }
+
+    @GetMapping("/api/routines")
+    public Collection<Routine> getAllRoutines() {
+        return routineService.getAllRoutines();
+    }
+
+    @GetMapping("/api/routines/{id}")
+    public ResponseEntity<Routine> getRoutineById(@PathVariable String id) {
+        Routine routine = routineService.getRoutineById(id);
+
+        if (routine != null) {
+            return ResponseEntity.ok(routine);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/api/routines")
+    public Routine createRoutine(@RequestBody Routine routine) {
+        return routineService.createRoutine(routine);
+    }
+
+    @PutMapping("/api/routines/{id}")
+    public Routine updateRoutine(@PathVariable String id, @RequestBody Routine updatedRoutine) {
+        return routineService.updateRoutine(id, updatedRoutine);
+    }
+
+    @DeleteMapping("/api/routines/{id}")
+    public ResponseEntity<Routine> deleteRoutine(@PathVariable String id) {
+        Routine routine = routineService.getRoutineById(id);
+
+        if (routine != null) {
+            routineService.deleteRoutine(id);
+            return ResponseEntity.ok(routine);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/api/routines/{id}")
+    public ResponseEntity<Routine> patchRoutine(@PathVariable String id, @RequestBody Map<String, Object> updates) {
+        Routine routine = routineService.patchRoutine(id, updates);
+
+        if (routine != null) {
+            return ResponseEntity.ok(routine);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
--- src/main/java/com/gymapp/gymapp/Service/RoutineService.java
+++ src/main/java/com/gymapp/gymapp/Service/RoutineService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Collection;
 
 @Service
 public class RoutineService {
