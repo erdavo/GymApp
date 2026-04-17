5a1b77ae99c796ef367f13f225cd468c1bc59f77
Borja <borjagonzare@gmail.com>
2026-04-17 12:05:28 +0200

    feat: add GET endpoint for retrieving all exercises

---
+++ src/main/java/com/gymapp/gymapp/Controller/ExerciseController.java
@@ -0,0 +1,25 @@
+package com.gymapp.gymapp.Controller;
+
+
+
+import com.gymapp.gymapp.Model.Exercise;
+import com.gymapp.gymapp.Service.ExerciseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    public ExerciseController(ExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+    @GetMapping("/api/exercises")
+    public List<Exercise> getAllExercises() {
+        return exerciseService.getAllExercises();
+    }
+}
---
+++ src/main/java/com/gymapp/gymapp/Service/ExerciseService.java
@@ -0,0 +1,22 @@
+package com.gymapp.gymapp.Service;
+
+import com.gymapp.gymapp.Model.Exercise;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ExerciseService {
+
+    private final List<Exercise> exercises = new ArrayList<>();
+
+    public ExerciseService() {
+        exercises.add(new Exercise("1", "Push Up", "Chest exercise", "Chest", "Medium"));
+        exercises.add(new Exercise("2", "Squat", "Leg exercise", "Legs", "Easy"));
+    }
+
+    public List<Exercise> getAllExercises() {
+        return exercises;
+    }
+}
