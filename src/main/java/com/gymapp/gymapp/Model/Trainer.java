3e495f7efbd371203816094f10813a77adec2e95
David <doto0992@gmail.com>
2026-04-17 17:51:10 +0200

    feat: added trainer model

---
+++ src/main/java/com/gymapp/gymapp/Model/Trainer.java
@@ -0,0 +1,44 @@
+package com.gymapp.gymapp.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trainer {
+    private String id;
+    private String name;
+    private String email;
+    private String specialty;
+    // private List<Routine> routines;   // 1:N con Routine
+
+    public Trainer(String id, String name, String email, String specialty) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.specialty = specialty;
+        // this.routines = new ArrayList<>();
+    }
+
+    // --- Getters & Setters ---
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSpecialty() { return specialty; }
+    public void setSpecialty(String specialty) { this.specialty = specialty; }
+
+    /* Todavia no porque para la tarea 1 las entidades no se relacionan entre si. Para la tarea 2 se relacionan.
+
+    public List<Routine> getRoutines() { return routines; }
+    public void setRoutines(List<Routine> routines) { this.routines = routines; }
+
+    public void addRoutine(Routine routine) {
+        this.routines.add(routine);
+    }
+    */
+}
