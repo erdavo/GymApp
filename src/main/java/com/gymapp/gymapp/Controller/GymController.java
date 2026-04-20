d018a874e0d7b7d11b0738fe4875a9d5670d5a5e
govmgonzalo <govmgonzalo@gmail.com>
2026-04-20 18:25:33 +0200

    feat: implemented edit, update and delete from exercise and routine

--- src/main/java/com/gymapp/gymapp/Controller/GymController.java
+++ src/main/java/com/gymapp/gymapp/Controller/GymController.java
@@ -10,6 +10,7 @@ import com.gymapp.gymapp.Service.ExerciseService;
 import com.gymapp.gymapp.Service.RoutineService;
 import com.gymapp.gymapp.Service.TrainerService;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.spec.RSAOtherPrimeInfo;
@@ -60,5 +61,48 @@ public class GymController {
         return "redirect:/#featured-routines";
     }
 
+    @GetMapping("/editExercise/{id}")
+    public String editExercisePage(@PathVariable Integer id, Model model) {
+        Exercise exercise = exerciseService.getExerciseById(id);
+        if (exercise != null) {
+            model.addAttribute("exercise", exercise);
+            return "editExercise";
+        }
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editRoutine/{id}")
+    public String editRoutinePage(@PathVariable Integer id, Model model) {
+        Routine routine = routineService.getRoutineById(id);
+        if (routine != null) {
+            model.addAttribute("routine", routine);
+            return "editRoutine";
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping("/exercises/update")
+    public String updateExercise(@ModelAttribute Exercise exercise) {
+        exerciseService.updateExercise(exercise.getId(), exercise);
+        return "redirect:/#popular-exercises";
+    }
+
+    @PostMapping("/routines/update")
+    public String updateRoutine(@ModelAttribute Routine routine) {
+        routineService.updateRoutine(routine.getId(), routine);
+        return "redirect:/#featured-routines";
+    }
+
+    @GetMapping("/exercises/delete/{id}")
+    public String deleteExercise(@PathVariable Integer id) {
+        exerciseService.deleteExercise(id);
+        return "redirect:/#popular-exercises";
+    }
+
+    @GetMapping("/routines/delete/{id}")
+    public String deleteRoutine(@PathVariable Integer id) {
+        routineService.deleteRoutine(id);
+        return "redirect:/#featured-routines";
+    }
 
 }
