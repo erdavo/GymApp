ae59dd932b793657c26fd7aafa6a6d95e7ff391a
garrido023 <ivangarridom2004@gmail.com>
2026-04-20 12:43:11 +0200

    feat: create TrainerController.java

--- src/main/java/com/gymapp/gymapp/Controller/TrainerController.java
+++ src/main/java/com/gymapp/gymapp/Controller/TrainerController.java
@@ -1,4 +1,59 @@
 package com.gymapp.gymapp.Controller;
 
+import com.gymapp.gymapp.Model.Trainer;
+import com.gymapp.gymapp.Service.TrainerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Map;
+
+@RestController
 public class TrainerController {
+    private final TrainerService trainerService;
+
+    public TrainerController(TrainerService trainerService) { this.trainerService = trainerService; }
+
+    @GetMapping("/api/trainers")
+    public Collection<Trainer> getAllTrainers() { return trainerService.getAllTrainers(); }
+
+    @GetMapping("/api/trainers/{id}")
+    public ResponseEntity<Trainer> getTrainerById(@PathVariable String id) {
+        Trainer trainer = trainerService.getTrainerById(id);
+
+        if (trainer != null) {
+            return ResponseEntity.ok(trainer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/api/trainers")
+    public Trainer createTrainer(@RequestBody Trainer trainer) { return trainerService.createTrainer(trainer); }
+
+    @PutMapping("/api/trainers/{id}")
+    public Trainer updateTrainer(@PathVariable String id, @RequestBody Trainer updatedTrainer) { return trainerService.updateTrainer(id, updatedTrainer);}
+
+    @DeleteMapping("/api/trainers/{id}")
+    public ResponseEntity<Trainer> deleteTrainer(@PathVariable String id) {
+        Trainer trainer = trainerService.getTrainerById(id);
+
+        if (trainer != null) {
+            trainerService.deleteTrainer(id);
+            return ResponseEntity.ok(trainer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/api/trainers/{id}")
+    public ResponseEntity<Trainer> patchTrainer(@PathVariable String id, @RequestBody Map<String, Object> updates) {
+        Trainer trainer = trainerService.patchTrainer(id, updates);
+
+        if (trainer != null) {
+            return ResponseEntity.ok(trainer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
