4723e4acf2908aae617d696a990ca7dc429fd48f
David <doto0992@gmail.com>
2026-04-20 11:25:33 +0200

    feat: add visual feedback and animations for input errors

--- src/main/resources/static/js/calculator.js
+++ src/main/resources/static/js/calculator.js
@@ -1,13 +1,39 @@
 function calculateBMR() {
     const gender = document.getElementById('gender').value;
-    const age = parseInt(document.getElementById('age').value);
-    const weight = parseFloat(document.getElementById('weight').value);
-    const height = parseFloat(document.getElementById('height').value);
+    const ageInput = document.getElementById('age');
+    const weightInput = document.getElementById('weight');
+    const heightInput = document.getElementById('height');
     const resultDiv = document.getElementById('bmr-result');
 
-    if (age > 0 && weight > 0 && height > 0) {
+
+    const arrayInputs = [ageInput, weightInput, heightInput];
+    arrayInputs.forEach(input => input.classList.remove('input-error'));
+
+    let isValid = true;
+
+    if (!ageInput.value || ageInput.value <= 0){
+        ageInput.classList.add("input-error");
+        isValid = false;
+    }
+
+    if (!weightInput.value || weightInput.value <= 0) {
+        weightInput.classList.add('input-error');
+        isValid = false;
+    }
+
+    if (!heightInput.value || heightInput.value <= 0) {
+        heightInput.classList.add('input-error');
+        isValid = false;
+    }
+
+
+    if (isValid) {
+        const age = parseInt(ageInput.value);
+        const weight = parseFloat(weightInput.value);
+        const height = parseFloat(heightInput.value);
+
         let bmr;
-        
+
         if (gender === 'male') {
             bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
         } else {
@@ -19,6 +45,6 @@ function calculateBMR() {
             <p>This is the energy your body needs at rest.</p>
         `;
     } else {
-        resultDiv.innerHTML = "<p>Please fill all fields correctly.</p>";
+        resultDiv.innerHTML = "<p style = 'color: #ff3b3b'>Please fill all fields correctly.</p>";
     }
 }
